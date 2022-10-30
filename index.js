778c1b24a79619a6ed1dbb87b69de012bf81a45c
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2022-10-29 18:33:28 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var arr=["one", "two", "three", "four"]
+console.log(arr);
+arr.push("five")
+console.log(arr);
+arr.pop()
+arr.pop()
+console.log(arr);
+console.log(arr.length);
+console.log(arr.reverse());
+console.log(arr.toString());
+console.log(arr.join("*"));
+arr.shift()
+console.log(arr);
+arr.unshift("five","six")
+console.log(arr);
+arr[3]="seven";
+console.log(arr);
