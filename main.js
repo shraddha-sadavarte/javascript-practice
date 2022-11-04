f0e9bd591626360572b95f4ce30bf032cd8c88b3
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2022-11-04 12:18:40 -0700

    map function

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ var student=["Sakshi","Divya","Mansi","Pranali"];
 student.forEach((item,index)=>{
     console.log("Sending email to "+ item +  " at index " + index);
 })
+
+var alphabets=["a","b","c","d"];
+
+alphabets.map((alphabet,index)=>{
+    console.log(alphabet);
+})
+
+var students=["Shravani","Yash","Dhiraj","Janki"];
+
+var stud=students.map((student,index)=>{
+    console.log(student + " is a science student");
+    if(index%2==0){
+        console.log(student + " is a brilliant student");
+    }
+    else{
+        console.log(student + " is a avrage student");
+    }
+})
